22948f242bc0abf572afa5731db9a5288f15ef48
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-11-09 01:51:49 -0800

    added pics and updated front page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import HomePage from './Homepage'
+
 
 function App() {
 
@@ -6,7 +8,7 @@ function App() {
 
   return (
     <>
-     
+     <HomePage />
      
     </>
   )
